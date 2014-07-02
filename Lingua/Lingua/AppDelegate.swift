c86ebfd8250d993d4cf57a7d161f7fb0e05eb4a2
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-07-02 12:10:03 +0700

    Client connects to Pusher Server

--- Lingua/Lingua/AppDelegate.swift
+++ Lingua/Lingua/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
         
+        PusherManager.sharedInstance.connectToPusher();
+        
         return true
     }
 
@@ -32,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.git 
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
--- Lingua/Lingua/Managers/PusherManager.swift
+++ Lingua/Lingua/Managers/PusherManager.swift
@@ -8,21 +8,50 @@
 
 import Foundation
 
+// Global constants
 let kPusherAPIKey = "f67750bab8e68e5f2409"
 let kPusherAPIID = "79890"
 let kPusherAPISecret = "a4b6be34cce67b8a8ec7"
+let kPuserAuthorizationURL = "http://pusher-chat-server.herokuapp.com/pusher/auth"
 
-class PusherManager : NSObject {
-//    var pusherClient : PTPusher
+
+class PusherManager : NSObject, PTPusherDelegate {
+    var pusherClient : PTPusher = PTPusher()
     
     class var sharedInstance : PusherManager {
-        struct Static {
-            static let instance : PusherManager = PusherManager()
+        get {
+            struct Static {
+                static let instance : PusherManager = PusherManager()
+            }
+            return Static.instance;
         }
-        return Static.instance;
     }
 
-//    init() {
-//        pusherClient = PTPusher.pusherWithKey(kPusherAPIKey, delegate: self, encrypted: true)
-//    }
+    init() {
+        super.init()
+        
+        pusherClient = PTPusher.pusherWithKey(kPusherAPIKey, delegate: self, encrypted: true) as PTPusher
+        pusherClient.authorizationURL = NSURL.URLWithString(kPuserAuthorizationURL)
+    }
+    
+    func connectToPusher() {
+        pusherClient.connect()
+    }
+    
+    
+    // MARK: PTPusherDelegate
+    
+    func pusher(pusher: PTPusher, connectionWillConnect connection: PTPusherConnection) -> Bool {
+        println("[pusher] Pusher client connecting...");
+        return true;
+    }
+    
+    func pusher(pusher: PTPusher, connectionDidConnect connection: PTPusherConnection) {
+        println("[pusher-\(connection.socketID)] Pusher client connected");
+    }
+    
+    func pusher(pusher: PTPusher, connection: PTPusherConnection, failedWithError error: NSError ) {
+        println("[pusher] Pusher Connection failed with error: \(error)");
+    }
+
 }
