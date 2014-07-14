09a1f9dbf27e54a83f76bbb65ee85a175193c026
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-07-14 11:59:22 +0700

    Add messages screen

--- Lingua/Lingua/Controllers/LINFriendListController.swift
+++ Lingua/Lingua/Controllers/LINFriendListController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class LINFriendListController: LINViewController {
 
+    @IBOutlet var tableView: UITableView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 70
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension LINFriendListController: UITableViewDataSource, UITableViewDelegate {
     
-
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("kFriendCellIdentifier") as UITableViewCell
+        
+        return cell
+    }
 }
---
+++ Lingua/Lingua/Views/LINFriendCell.swift
@@ -0,0 +1,17 @@
+//
+//  LINFriendCell.swift
+//  Lingua
+//
+//  Created by Kiet Nguyen on 7/13/14.
+//  Copyright (c) 2014 2359Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LINFriendCell: UITableViewCell {
+    @IBOutlet var avatarImgView: UIImageView
+    @IBOutlet var nameLabel: UILabel
+    @IBOutlet var roleName: UILabel
+    @IBOutlet var dateLabel: UILabel
+}
