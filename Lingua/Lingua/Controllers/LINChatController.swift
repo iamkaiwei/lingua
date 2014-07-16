cef0fd1f0133c4ddbcc5adc9ed6df6b4085c9321
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-07-16 15:48:15 +0700

    Add border for input view and auto resize input textview

--- Lingua/Lingua/Controllers/LINChatController.swift
+++ Lingua/Lingua/Controllers/LINChatController.swift
@@ -7,11 +7,17 @@
 //
 
 import Foundation
+import QuartzCore
 
 class LINChatController: UIViewController {
+
+    @IBOutlet var inputContainerView: UIView
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputContainerView.layer.borderColor = UIColor(red: 153.0/255, green: 153.0/255, blue: 153.0/255, alpha: 1.0).CGColor
+        inputContainerView.layer.borderWidth = 0.5
     }
     
     @IBAction func backButtonTouched(sender: UIButton) {
