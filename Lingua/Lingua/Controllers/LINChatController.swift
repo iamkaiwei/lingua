8ca755a6fba9dd175199939555d04b98fdd0232e
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-07-17 00:10:47 +0700

    Move input text when keyboard appears

--- Lingua/Lingua/Controllers/LINChatController.swift
+++ Lingua/Lingua/Controllers/LINChatController.swift
@@ -12,13 +12,53 @@ import QuartzCore
 class LINChatController: UIViewController {
 
     @IBOutlet var inputContainerView: UIView
+    @IBOutlet var inputTextView: UITextView
+    @IBOutlet var speakButton: UIButton
+    @IBOutlet var sendButton: UIButton
+    @IBOutlet var titleNavLabel: UILabel
+    @IBOutlet var tableView: UITableView
+    
+    @IBOutlet var inputContainerViewBottomLayoutGuideConstraint: NSLayoutConstraint
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureInputContainerView()
+        configureTapGestureOnTableView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillShowNotification:",
+            name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillHideNotification:",
+            name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    // MARK: Configuration
+    
+    func configureInputContainerView () {
         inputContainerView.layer.borderColor = UIColor(red: 153.0/255, green: 153.0/255, blue: 153.0/255, alpha: 1.0).CGColor
         inputContainerView.layer.borderWidth = 0.5
     }
+    
+    func configureTapGestureOnTableView() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: "didTapOnTableView:")
+        tableView.addGestureRecognizer(tapGesture)
+    }
+    
+    // MARK: Actions
     
     @IBAction func backButtonTouched(sender: UIButton) {
         if navigationController {
@@ -26,5 +66,60 @@ class LINChatController: UIViewController {
         } else {
             dismissViewControllerAnimated(true, completion: nil)
        }
+    }
+    
+    func didTapOnTableView(sender: UITapGestureRecognizer) {
+        inputTextView.resignFirstResponder()
+    }
+    
+    // MARK: Keyboard Events Notifications
+    
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showKeyboard: true)
+    }
+    
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showKeyboard: false)
+    }
+    
+    func keyboardWillChangeFrameWithNotification(notfication: NSNotification, showKeyboard: Bool) {
+        let userInfo = notfication.userInfo
+        let kbSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue().size
+        var animationDuration: NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber).doubleValue
+
+        if showKeyboard {
+            // Convert the keyboard frame from screen to view coordinates.
+            let keyboardScreenBeginFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue()
+            let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+            
+            let keyboardViewBeginFrame = view.convertRect(keyboardScreenBeginFrame, fromView: view.window)
+            let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+            let originDelta = keyboardViewEndFrame.origin.y - keyboardViewBeginFrame.origin.y
+            
+            // The input container view should be adjusted, update the constant for this constraint.
+            self.inputContainerViewBottomLayoutGuideConstraint.constant -= originDelta
+        } else {
+            self.inputContainerViewBottomLayoutGuideConstraint.constant = 0
+        }
+        
+        view.setNeedsUpdateConstraints()
+    
+        UIView.animateWithDuration(animationDuration, animations: {
+            var inputContainerFrame = self.inputContainerView.frame
+            var tableFrame = self.tableView.frame
+            
+            if showKeyboard {
+                inputContainerFrame.origin.y -= kbSize.height
+                tableFrame.size.height -= kbSize.height
+            } else {
+                inputContainerFrame.origin.y += kbSize.height
+                tableFrame.size.height += kbSize.height
+            }
+            
+            self.inputContainerView.frame = inputContainerFrame
+            self.tableView.frame = tableFrame
+            
+            self.view.layoutIfNeeded()
+        })
     }
 }
